0bc62dbd2aed49e0c3ba6aa5c8e63d4d8bc42224
Flash3241 <108479650+Flash3241@users.noreply.github.com>
2022-09-17 16:54:54 -0700

    Update sudokusolver.java

--- sudokusolver.java
+++ sudokusolver.java
@@ -1,5 +1,6 @@
 public class sudokusolver {
     public int[][] grid;
+
     public void dokugrid(){
         grid=new int[9][9];
         for (int i=0; i<9; i++) {
@@ -8,8 +9,37 @@ public class sudokusolver {
             }
         }
     }
+    public int startrow(int row){
+        if(row<3){
+            return 0;
+        }
+        if(row>=3 && row<6){
+            return 3;
+        }
+        if(row>=6 && row<9){
+            return 6;
+        }
+        else return 0;
+    }
+
+    public int startcol(int col){
+        if(col<3){
+            return 0;
+        }
+        if(col>=3 && col<6){
+            return 3;
+        }
+        if(col>=6 && col<9){
+            return 6;
+        }
+        else return 0;
+    }
+
     public boolean valid(int num, int row, int col){
         boolean answer = false;
+        if(row>=9 || col>=9){
+            return false;
+        }
         for(int i =0; i<9;i++){
             if(grid[row][i]==num){
                 answer=false;
@@ -26,9 +56,19 @@ public class sudokusolver {
                 answer=true;
             }
         }
+        int boxrow=startrow(row);
+        int boxcol=startcol(col);
+        for(int i=0; i<3;i++){
+            for(int j=0;j<3;j++){
+                if(grid[boxrow+i][boxcol+j]==num){
+                    answer=false;
+                    break;
+                }
+            }
+        }
         return answer;
     }
-    public  insert(int num, int row, int col){
+    public void insert(int num, int row, int col){
         if(valid(num, row, col)==true){
             grid[row][col]=num;
         }
