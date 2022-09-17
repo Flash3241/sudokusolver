86934d7b0bcc3a3728db0eaba3c9ea219929fe63
Flash3241 <108479650+Flash3241@users.noreply.github.com>
2022-09-17 16:26:37 -0700

    Add files via upload

---
+++ sudokusolver.java
@@ -0,0 +1,18 @@
+public class sudokusolver {
+    public int[][] grid;
+    public void dokugrid(){
+        grid=new int[9][9];
+        for (int i=0; i<9; i++) {
+            for (int j=0; j<9; j++) {
+                grid[i][j]= 0;
+            }
+        }
+    }
+    public void insert(int num; int row; int col){
+        grid[row][col]=num;
+    }
+    public in
+    public int[][] solvegrid(){
+
+    }
+}
