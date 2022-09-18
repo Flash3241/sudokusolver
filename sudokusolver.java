6be272d55d6c4cf092401e00fabb10f6ed76ef9a
Flash3241 <108479650+Flash3241@users.noreply.github.com>
2022-09-18 15:05:12 -0700

    Update sudokusolver.java

--- sudokusolver.java
+++ sudokusolver.java
@@ -78,39 +78,10 @@ public class sudokusolver {
             grid[row][col]=num;
         }
     }
-    public ArrayList<Integer> possiblesol(int row, int col){
-        ArrayList<Integer> solutions= new ArrayList<Integer>();
-        solutions.add(1);
-        solutions.add(2);
-        solutions.add(3);
-        solutions.add(4);
-        solutions.add(5);
-        solutions.add(6);
-        solutions.add(7);
-        solutions.add(8);
-        solutions.add(9);
-        for(int i=0; i<9;i++){
-            for(int j=1; j<=9;j++){
-                if(grid[row][i]==j && solutions.contains(j)){
-                    solutions.remove(j);
-                }
-                if(grid[i][col]==j && solutions.contains(j)){
-                    solutions.remove(j);
-                }
-            }
+    public static void printarray(ArrayList<Integer> list){
+        for(int i=0; i<list.size();i++){
+            System.out.print(list.get(i));
         }
-        int boxrow=startrow(row);
-        int boxcol=startcol(col);
-        for(int i=0; i<3;i++){
-            for(int j=0;j<3;j++){
-                for(int s=1; s<=9;s++){
-                    if(grid[boxrow+i][boxcol+j]==s && solutions.contains(s)){
-                        solutions.remove(s);
-                    }
-                }
-            }
-        }
-        return solutions;
     }
     public static void printgrid(int[][] newgrid){
         String line = "___________________";
@@ -124,6 +95,119 @@ public class sudokusolver {
         }
         System.out.println(line);
     }
+    public static ArrayList<Integer> rowsol(int[][] board, int row){
+        ArrayList<Integer> possol=new ArrayList<Integer>();
+        possol.add(1);
+        possol.add(2);
+        possol.add(3);
+        possol.add(4);
+        possol.add(5);
+        possol.add(6);
+        possol.add(7);
+        possol.add(8);
+        possol.add(9);
+        for(int i=0; i<9;i++){
+            if(possol.contains(board[row][i])){
+                possol.remove(board[row][i]);
+            }
+        }
+        return possol;
+    }
+    public static ArrayList<Integer> colsol(int[][] board, int col){
+        ArrayList<Integer> possol=new ArrayList<Integer>();
+        possol.add(1);
+        possol.add(2);
+        possol.add(3);
+        possol.add(4);
+        possol.add(5);
+        possol.add(6);
+        possol.add(7);
+        possol.add(8);
+        possol.add(9);
+        for(int i=0; i<9;i++){
+            if(possol.contains(board[i][col])){
+                possol.remove(board[i][col]);
+            }
+        }
+        return possol;
+    }
+
+
+    public static int startrow2(int row){
+        if(row<3){
+            return 0;
+        }
+        if(row>=3 && row<6){
+            return 3;
+        }
+        if(row>=6 && row<9){
+            return 6;
+        }
+        else return 0;
+    }
+
+    public static int startcol2(int col){
+        if(col<3){
+            return 0;
+        }
+        if(col>=3 && col<6){
+            return 3;
+        }
+        if(col>=6 && col<9){
+            return 6;
+        }
+        else return 0;
+    }
+    public static ArrayList<Integer> boxsol(int[][] board, int row, int col){
+        ArrayList<Integer> possol=new ArrayList<Integer>();
+        possol.add(1);
+        possol.add(2);
+        possol.add(3);
+        possol.add(4);
+        possol.add(5);
+        possol.add(6);
+        possol.add(7);
+        possol.add(8);
+        possol.add(9);
+        int startrow=startrow2(row);
+        int startcol=startcol2(col);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(possol.contains(board[startrow+i][startcol+j])){
+                    possol.remove(board[startrow+i][startcol+j]);
+                }
+            }
+        }
+        return possol;
+    }
+
+    public static int[][] solve(int[][] board){
+        int[][] newboard=board;
+        ArrayList<Integer> rowlist=new ArrayList<Integer>();
+        ArrayList<Integer> collist=new ArrayList<Integer>();
+        ArrayList<Integer> boxlist=new ArrayList<Integer>();
+        for(int i=0; i<9;i++){
+            for(int j=0;j<9;j++){
+                if(newboard[i][j]==0){
+                    rowlist=rowsol(board,i);
+                    collist=colsol(board, j);
+                    boxlist=boxsol(board, i, j);
+                    for(int sol=0; sol<rowlist.size();sol++){
+                        if(collist.contains(rowlist.get(sol))){
+                            collist.remove(rowlist.get(sol));
+                        }
+                        if(collist.contains(boxlist.get(sol))){
+                            collist.remove(boxlist.get(sol));
+                        }
+                    }
+                    if(collist.size()==1){
+                        newboard[i][j]=collist.get(0);
+                    }
+                }
+            }
+        }
+        return newboard;
+    }
 
     public static void main(String[] args) {
         if (unitTests()) {
@@ -132,7 +216,19 @@ public class sudokusolver {
             System.out.println("ERROR: Failed test.\n");
             return;
         }
+        int[][] ex={
+            {0,3,0,1,7,2,8,4,5},
+            {4,8,1,9,0,0,2,0,3},
+            {7,5,0,0,8,4,1,9,6},
+            {2,0,5,0,0,0,0,8,0},
+            {0,1,0,0,2,3,7,6,0},
+            {0,6,4,0,0,8,5,2,9},
+            {5,9,0,2,3,7,4,1,0},
+            {0,0,0,4,0,1,0,5,0},
+            {1,4,0,0,9,5,6,3,0}
+        };
         String solveornot="solve or insert";
+        String exit = "enter 0 if want to solve, any number otherwise";
         String enternum ="enter number";
         String enterrow ="enter row";
         String entercol ="enter column";
@@ -141,12 +237,10 @@ public class sudokusolver {
         sudokusolver sudoku = new sudokusolver();
         Scanner scanner = new Scanner(System.in);
         // sudoku.insert(3,2,1);
-        printgrid(sudoku.grid);
+        printgrid(ex);
         System.out.println(solveornot);
         String answer = scanner.nextLine();
-        System.out.println(answer);
-        System.out.println(ans);
-        while(answer==ans){
+        while(answer.equals(ans)){
             System.out.println(enternum);
             int num=scanner.nextInt();
             System.out.println(enterrow);
@@ -155,12 +249,18 @@ public class sudokusolver {
             int col=scanner.nextInt();
             sudoku.insert(num, row, col);
             printgrid(sudoku.grid);
-            System.out.println(solveornot);
-            answer=scanner.nextLine();
+            System.out.println(exit);
+            int answer2=scanner.nextInt();
+            if(answer2==0){
+                break;
+            }
         }
+        printgrid(solve(ex));
+
         scanner.close();
     }
-    public static boolean unitTests() {
+    public static boolean unitTests(
+    ) {
         return true;
     }
 
@@ -216,4 +316,38 @@ public class sudokusolver {
     //     }
     // }
     
+    // public ArrayList<Integer> possiblesol(int row, int col){
+    //     ArrayList<Integer> solutions= new ArrayList<Integer>();
+    //     solutions.add(1);
+    //     solutions.add(2);
+    //     solutions.add(3);
+    //     solutions.add(4);
+    //     solutions.add(5);
+    //     solutions.add(6);
+    //     solutions.add(7);
+    //     solutions.add(8);
+    //     solutions.add(9);
+    //     for(int i=0; i<9;i++){
+    //         for(int j=1; j<=9;j++){
+    //             if(grid[row][i]==j && solutions.contains(j)){
+    //                 solutions.remove(j);
+    //             }
+    //             if(grid[i][col]==j && solutions.contains(j)){
+    //                 solutions.remove(j);
+    //             }
+    //         }
+    //     }
+    //     int boxrow=startrow(row);
+    //     int boxcol=startcol(col);
+    //     for(int i=0; i<3;i++){
+    //         for(int j=0;j<3;j++){
+    //             for(int s=1; s<=9;s++){
+    //                 if(grid[boxrow+i][boxcol+j]==s && solutions.contains(s)){
+    //                     solutions.remove(s);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return solutions;
+    // }
 }
